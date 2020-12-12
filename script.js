77f94c83f9c3355645b02859afd349c5ad98be9f
Bre Boersma <breoersma@gmail.com>
2020-12-12 09:49:01 -0500

    style canges

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
 function moveHands() {
-    with (new Date ()) {
-        h = 30  ((getHours() % 12) + getMinutes() / 60); // 30 degrees hour
-        m = 6 * getMinutes(); // 6 degree every minute
+    with (new Date()) {
+        h = 30 * ((getHours() % 12) + getMinutes() / 60); // 30 degrees hour
+        m = 6 * getMinutes(); // 6 degrees every minute
         s = 6 * getSeconds(); // 6 degrees every second
-        // setting the rotate css attribute to those degree values -->
-        document.getElementById("seconds").style.cssText =
-             "-webkit-transform:rotate(" + s + "deg);";
-        document.getElementById("minutes").style.cssText =
-            "-webkit-transform:rotate(" + s + "deg);";
-         document.getElementById("hours").style.cssFloat.cssText =
-             "-webkit-transform:rotate(" + h + "deg);";
-        
-         setTimeout(moveHands, 1000); // calling the function every second
+        document.getElementById("seconds").style.cssText = "-webkit-transform:rotate(" + s + "deg);";
+        document.getElementById("minutes").style.cssText = "-webkit-transform:rotate(" + m + "deg);";
+        document.getElementById("hours").style.cssText = "-webkit-transform:rotate(" + h + "deg);";
+
+        setTimeout(moveHands, 1000);
     }
 }
 
-window.onload = moveHands; // making sure the function starts on load of webpage
+window.onload = moveHands;
 
   function displayclock () {
     var time = new Date();
